15fd9122759545453eb8c48babcd7ee3fa4cb02d
unknown <chaitalykundu@gmail.com>
2020-03-27 21:55:10 +0530

    changed message in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Initial Application
                 </div>
 
                 <div class="links">
